601c936352b8cd3c47465b4f8bf0b8c101167134
Sobhan369 <sobhanbabukarri@gmail.com>
2025-09-01 16:22:04 +0530

    updated

---
+++ server.js
@@ -0,0 +1,65 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+    const extname = path.extname(filePath);
+    let contentType = 'text/html';
+
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.gif':
+            contentType = 'image/gif';
+            break;
+        case '.svg':
+            contentType = 'image/svg+xml';
+            break;
+        case '.txt':
+            contentType = 'text/plain';
+            break;
+        case '.woff':
+            contentType = 'font/woff';
+            break;
+        case '.woff2':
+            contentType = 'font/woff2';
+            break;
+        default:
+            contentType = 'text/html';
+    }
+
+    fs.readFile(filePath, (error, content) => {
+        if (error) {
+            if (error.code === 'ENOENT') {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end('<h1>404 Not Found</h1>', 'utf-8');
+            } else {
+                res.writeHead(500);
+                res.end('Sorry, there was an error: ' + error.code + ' ..\n');
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
